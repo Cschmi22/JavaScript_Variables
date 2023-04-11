19ae104da8897704b0c672c0f9eaba27c87fcd70
Caleb Schmidt <cschmi14@gmail.com>
2023-04-11 10:24:39 -0400

    Done

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Mark";
+let lastName = "Buffalo";
+var age = 32;
 
-// Exercise 2
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
